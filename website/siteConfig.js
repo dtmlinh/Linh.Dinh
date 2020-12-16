1c13dfbf811288bde389a05a5860c2a941a03b32
dtmlinh <linh.t.m.dinh@gmail.com>
2020-12-16 16:09:21 -0600

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,13 +16,13 @@ const siteConfig = {
     tagline: 'Aspiring data scientist, passionate about public health and environmental issues.',
     profilePhoto: 'img/profile.png',
     url: 'https://dtmlinh.github.io', // Your website URL
-    baseUrl: '/pages/', // Base URL for your project */
+    baseUrl: '/bio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
-    projectName: 'pages',
+    projectName: 'bio',
     organizationName: 'dtmlinh',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
